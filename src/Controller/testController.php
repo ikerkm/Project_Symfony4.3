a53bc1bdfd9c905cb39bf5aa562bdde7f916bb7e
iker <ikerkuadra@gmail.com>
2019-07-02 11:33:58 +0200

    Configured, routing, and templates

---
+++ src/Controller/testController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class testController extends AbstractController
+{
+    /**
+     * @Route("/random")
+     * 
+     *      */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render(
+            'home_test.html.twig',
+            ['number' => $number]
+        );
+    }
+}
